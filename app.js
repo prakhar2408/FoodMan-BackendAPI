736ffd7854f6df8da4016dd0248c2d208ead59cb
Prakhar Agrawal <prakharagrawal2408@gmail.com>
2021-11-12 18:53:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const CustomerRoutes = require("./routes/CustomerRoutes");
+const RestaurentRoutes = require("./routes/RestaurentRoutes");
+
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/customer", CustomerRoutes);
+app.use("/restaurent", RestaurentRoutes);
+mongoose
+  .connect(
+    "mongodb://hackfooduser:hack123456789@cluster0-shard-00-00.fsvp3.mongodb.net:27017,cluster0-shard-00-01.fsvp3.mongodb.net:27017,cluster0-shard-00-02.fsvp3.mongodb.net:27017/Database?ssl=true&replicaSet=atlas-plnyra-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    console.log("connected");
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
