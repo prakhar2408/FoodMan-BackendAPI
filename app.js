0ae2af4c98b51d8e9e0f4be464d64b26c0029c0a
Prakhar Agrawal <prakharagrawal2408@gmail.com>
2022-05-30 22:26:25 +0530

    Progress Till now

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const RestaurentRoutes = require("./routes/RestaurentRoutes");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
